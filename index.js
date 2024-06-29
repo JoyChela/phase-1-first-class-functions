6840405a13407cb6e7e279c336ea35a6addf22d9
Joy Chela <joychela000@gmail.com>
2024-06-29 14:19:32 +0300

    first class

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function receivesAFunction(back){
+    back();
+
+}
+
+function returnsANamedFunction(){
+    return function exercise(){
+
+    };
+
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    };
+
+}
+
+
